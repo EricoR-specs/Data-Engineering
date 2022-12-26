fe4d9f88658f279cbea541d54615220ad0624946
EricoR-specs <82165040+EricoR-specs@users.noreply.github.com>
2022-12-26 08:08:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ with open ("prediksi_harga_rumah.pkl", "rb") as f:
 
 def prediksi(LT,LB,JKT,JKM,GRS):
     predict = pd.DataFrame()
-    predict['Luas Tanah'] = [LT]
-    predict['Luas Bangunan'] = [LB]
-    predict['Jumlah Kamar Tidur'] = [JKT]
-    predict['Jumlah Kamar Mandi'] = [JKM]
-    predict['Garasi'] = [GRS]
+    predict['Luas Tanah'] = LT
+    predict['Luas Bangunan'] = LB
+    predict['Jumlah Kamar Tidur'] = JKT
+    predict['Jumlah Kamar Mandi'] = JKM
+    predict['Garasi'] = GRS
     return(model.predict(predict)[0])   
 
 lt = st.number_input("Luas Tanah")
